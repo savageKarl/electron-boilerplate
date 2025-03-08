f213a19acd09beb3c5135f996d301dc1b2569615
savage <2443005004@qq.com>
2025-03-09 01:47:53 +0800

    Add Commitizen for conventional commit messages

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能
+        'fix', // 修复bug
+        'docs', // 文档变更
+        'style', // 代码格式（不影响代码运行的变动）
+        'refactor', // 重构（既不是新增功能，也不是修改bug的代码变动）
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build', // 打包
+        'ci' // CI相关变更
+      ]
+    ],
+    'subject-case': [0] // 关闭大小写检查
+  }
+};
